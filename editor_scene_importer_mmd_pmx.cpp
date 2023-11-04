a4565c1ab037682bdef66c0c814c71515441d1e5
K. S. Ernest (iFire) Lee <fire@users.noreply.github.com>
2023-11-04 07:04:08 -0700

    Refactor EditorSceneImporterMMDPMX class
    - Updated method signatures to match coding convention.

--- editor_scene_importer_mmd_pmx.cpp
+++ editor_scene_importer_mmd_pmx.cpp
@@ -64,10 +64,10 @@ Node *EditorSceneImporterMMDPMX::import_scene(const String &p_path, uint32_t p_f
 	return import_mmd_pmx_scene(p_path, p_flags, (float)p_options["animation/fps"]);
 }
 
-bool EditorSceneImporterMMDPMX::is_valid_index(mmd_pmx_t::sized_index_t *index) const {
-	ERR_FAIL_NULL_V(index, false);
-	int64_t bone_index = index->value();
-	switch (index->size()) {
+bool EditorSceneImporterMMDPMX::is_valid_index(mmd_pmx_t::sized_index_t *p_index) const {
+	ERR_FAIL_NULL_V(p_index, false);
+	int64_t bone_index = p_index->value();
+	switch (p_index->size()) {
 		case 1:
 			return bone_index < UINT8_MAX;
 		case 2:
@@ -78,23 +78,23 @@ bool EditorSceneImporterMMDPMX::is_valid_index(mmd_pmx_t::sized_index_t *index) 
 	}
 }
 
-void EditorSceneImporterMMDPMX::add_vertex(Ref<SurfaceTool> surface, mmd_pmx_t::vertex_t *vertex) const {
-	ERR_FAIL_NULL(surface);
-	ERR_FAIL_NULL(vertex);
-	ERR_FAIL_NULL(vertex->normal());
-	Vector3 normal = Vector3(vertex->normal()->x(),
-			vertex->normal()->y(),
-			vertex->normal()->z());
-	surface->set_normal(normal);
+void EditorSceneImporterMMDPMX::add_vertex(Ref<SurfaceTool> p_surface, mmd_pmx_t::vertex_t *r_vertex) const {
+	ERR_FAIL_NULL(p_surface);
+	ERR_FAIL_NULL(r_vertex);
+	ERR_FAIL_NULL(r_vertex->normal());
+	Vector3 normal = Vector3(r_vertex->normal()->x(),
+			r_vertex->normal()->y(),
+			r_vertex->normal()->z());
+	p_surface->set_normal(normal);
 	normal.z = -normal.z;
-	ERR_FAIL_NULL(vertex->uv());
-	Vector2 uv = Vector2(vertex->uv()->x(),
-			vertex->uv()->y());
-	surface->set_uv(uv);
-	ERR_FAIL_NULL(vertex->position());
-	Vector3 point = Vector3(vertex->position()->x() * mmd_unit_conversion,
-			vertex->position()->y() * mmd_unit_conversion,
-			vertex->position()->z() * mmd_unit_conversion);
+	ERR_FAIL_NULL(r_vertex->uv());
+	Vector2 uv = Vector2(r_vertex->uv()->x(),
+			r_vertex->uv()->y());
+	p_surface->set_uv(uv);
+	ERR_FAIL_NULL(r_vertex->position());
+	Vector3 point = Vector3(r_vertex->position()->x() * mmd_unit_conversion,
+			r_vertex->position()->y() * mmd_unit_conversion,
+			r_vertex->position()->z() * mmd_unit_conversion);
 	point.z = -point.z;
 	PackedInt32Array bones;
 	bones.push_back(0);
@@ -106,11 +106,11 @@ void EditorSceneImporterMMDPMX::add_vertex(Ref<SurfaceTool> surface, mmd_pmx_t::
 	weights.push_back(0.0f);
 	weights.push_back(0.0f);
 	weights.push_back(0.0f);
-	if (!vertex->_is_null_skin_weights()) {
-		mmd_pmx_t::bone_type_t bone_type = vertex->type();
+	if (!r_vertex->_is_null_skin_weights()) {
+		mmd_pmx_t::bone_type_t bone_type = r_vertex->type();
 		switch (bone_type) {
 			case mmd_pmx_t::BONE_TYPE_BDEF1: {
-				mmd_pmx_t::bdef1_weights_t *pmx_weights = (mmd_pmx_t::bdef1_weights_t *)vertex->skin_weights();
+				mmd_pmx_t::bdef1_weights_t *pmx_weights = (mmd_pmx_t::bdef1_weights_t *)r_vertex->skin_weights();
 				ERR_FAIL_NULL(pmx_weights);
 				if (is_valid_index(pmx_weights->bone_index())) {
 					bones.write[0] = pmx_weights->bone_index()->value();
@@ -118,7 +118,7 @@ void EditorSceneImporterMMDPMX::add_vertex(Ref<SurfaceTool> surface, mmd_pmx_t::
 				}
 			} break;
 			case mmd_pmx_t::BONE_TYPE_BDEF2: {
-				mmd_pmx_t::bdef2_weights_t *pmx_weights = (mmd_pmx_t::bdef2_weights_t *)vertex->skin_weights();
+				mmd_pmx_t::bdef2_weights_t *pmx_weights = (mmd_pmx_t::bdef2_weights_t *)r_vertex->skin_weights();
 				ERR_FAIL_NULL(pmx_weights);
 				for (uint32_t count = 0; count < 2; count++) {
 					if (is_valid_index(pmx_weights->bone_indices()->at(count).get())) {
@@ -128,7 +128,7 @@ void EditorSceneImporterMMDPMX::add_vertex(Ref<SurfaceTool> surface, mmd_pmx_t::
 				}
 			} break;
 			case mmd_pmx_t::BONE_TYPE_BDEF4: {
-				mmd_pmx_t::bdef4_weights_t *pmx_weights = (mmd_pmx_t::bdef4_weights_t *)vertex->skin_weights();
+				mmd_pmx_t::bdef4_weights_t *pmx_weights = (mmd_pmx_t::bdef4_weights_t *)r_vertex->skin_weights();
 				ERR_FAIL_NULL(pmx_weights);
 				for (uint32_t count = 0; count < RS::ARRAY_WEIGHTS_SIZE; count++) {
 					if (is_valid_index(pmx_weights->bone_indices()->at(count).get())) {
@@ -139,7 +139,7 @@ void EditorSceneImporterMMDPMX::add_vertex(Ref<SurfaceTool> surface, mmd_pmx_t::
 			} break;
 			case mmd_pmx_t::BONE_TYPE_SDEF: {
 				// TODO implement 2021-09-10 fire
-				mmd_pmx_t::sdef_weights_t *pmx_weights = (mmd_pmx_t::sdef_weights_t *)vertex->skin_weights();
+				mmd_pmx_t::sdef_weights_t *pmx_weights = (mmd_pmx_t::sdef_weights_t *)r_vertex->skin_weights();
 				ERR_FAIL_NULL(pmx_weights);
 				for (uint32_t count = 0; count < 2; count++) {
 					if (is_valid_index(pmx_weights->bone_indices()->at(count).get())) {
@@ -151,7 +151,7 @@ void EditorSceneImporterMMDPMX::add_vertex(Ref<SurfaceTool> surface, mmd_pmx_t::
 			case mmd_pmx_t::BONE_TYPE_QDEF:
 			default: {
 				// TODO implement 2021-09-10 fire
-				mmd_pmx_t::qdef_weights_t *pmx_weights = (mmd_pmx_t::qdef_weights_t *)vertex->skin_weights();
+				mmd_pmx_t::qdef_weights_t *pmx_weights = (mmd_pmx_t::qdef_weights_t *)r_vertex->skin_weights();
 				ERR_FAIL_NULL(pmx_weights);
 				ERR_FAIL_NULL(pmx_weights->bone_indices());
 				ERR_FAIL_NULL(pmx_weights->weights());
@@ -166,7 +166,7 @@ void EditorSceneImporterMMDPMX::add_vertex(Ref<SurfaceTool> surface, mmd_pmx_t::
 				}
 			} break;
 		}
-		surface->set_bones(bones);
+		p_surface->set_bones(bones);
 		real_t renorm = weights[0] + weights[1] + weights[2] + weights[3];
 		if (renorm != 0.0 && renorm != 1.0) {
 			weights.write[0] /= renorm;
@@ -174,20 +174,20 @@ void EditorSceneImporterMMDPMX::add_vertex(Ref<SurfaceTool> surface, mmd_pmx_t::
 			weights.write[2] /= renorm;
 			weights.write[3] /= renorm;
 		}
-		surface->set_weights(weights);
-		surface->add_vertex(point);
+		p_surface->set_weights(weights);
+		p_surface->add_vertex(point);
 	}
 }
 
-String EditorSceneImporterMMDPMX::convert_string(const std::string &s, uint8_t encoding) const {
+String EditorSceneImporterMMDPMX::convert_string(const std::string &p_string, uint8_t p_encoding) const {
 	String output;
-	if (encoding == 0) {
+	if (p_encoding == 0) {
 		Vector<char16_t> buf;
-		buf.resize(s.length() / 2);
-		memcpy(buf.ptrw(), s.c_str(), s.length() / 2 * sizeof(char16_t));
+		buf.resize(p_string.length() / 2);
+		memcpy(buf.ptrw(), p_string.c_str(), p_string.length() / 2 * sizeof(char16_t));
 		output.parse_utf16(buf.ptr(), buf.size());
 	} else {
-		output.parse_utf8(s.data(), s.length());
+		output.parse_utf8(p_string.data(), p_string.length());
 	}
 	return output;
 }
